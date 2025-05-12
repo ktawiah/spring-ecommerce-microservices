a82b1d45af3405c11e7fb4ae7beabe285b28e231
Kelvin <kelvintawiah224@gmail.com>
2025-05-12 16:52:49 -0500

    add configuration and discovery server config

--- discovery/src/test/java/dev/msecommerce/discovery/DiscoveryApplicationTests.java
+++ discovery/src/test/java/dev/msecommerce/discovery/DiscoveryApplicationTests.java
@@ -1,13 +1,13 @@
-package dev.msecommerce.configserver;
+package dev.msecommerce.discovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class DiscoveryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
