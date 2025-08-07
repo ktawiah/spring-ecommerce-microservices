bb7f0928ebbfe2aab29026137e66321e6c2e543f
Kelvin Tawiah <kelvintawiah224@gmail.com>
2025-08-07 17:05:29 -0500

    feat: add application configuration for RestTemplate and JPA auditing

---
+++ order/src/main/java/dev/msecommerce/order/config/AppConfig.java
@@ -0,0 +1,16 @@
+package dev.msecommerce.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableJpaAuditing
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
